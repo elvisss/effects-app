0603dafe88f8b6226c2d30dc38605cbd9bfe9ea2
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2019-06-08 11:57:38 -0500

    app without effects

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+// Modules personalized
+import { SharedModule } from './shared/shared.module';
+import { UsersModule } from './users/users.module';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    UsersModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/users/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { User } from 'src/app/models/user.model';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styles: []
+})
+export class ListComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor( private userService: UserService ) { }
+
+  ngOnInit() {
+    this.userService.getUsers()
+      .subscribe( data => {
+        this.users = data;
+      });
+  }
+
+}
